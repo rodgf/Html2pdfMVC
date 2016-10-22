9f2958fd5fda919c35a3642208d20bb5981c4e4a
rodgf <rodgf@yahoo.com>
2016-10-22 00:07:35 -0200

    Inicial

---
+++ Html2pdfMVC/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Html2pdfMVC.Controllers {
+  public class DefaultController : Controller {
+
+    // GET: Default
+    public ActionResult Index() {
+      int inID = (new Random()).Next(100);
+
+      ViewData["ID"] = inID;
+      return View();
+    }
+  }
+}
